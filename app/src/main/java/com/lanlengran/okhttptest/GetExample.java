2626f9eaa4d930548501ee28a11dbe1eb0bc5c69
qin <1392467757@qq.com>
2019-04-24 23:14:27 +0800

    首次提交项目

---
+++ app/src/main/java/com/lanlengran/okhttptest/GetExample.java
@@ -0,0 +1,16 @@
+package com.lanlengran.okhttptest;
+
+import com.squareup.okhttp.OkHttpClient;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetExample {
+
+
+  public static void main(String[] args) throws IOException {
+
+  }
+}
